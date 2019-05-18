5f915208841db0c06a9e33ffcf01fe4ac16287cd
Camilo Enriquez <rot7447@gmail.com>
2019-05-18 00:08:30 -0500

    Creating viewers and participants

---
+++ src/application/BannerController.java
@@ -0,0 +1,97 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class BannerController implements Initializable{
+
+    @FXML
+    private Tab tab1;
+    @FXML
+    private TextField path;
+    @FXML
+    private Label loadState;
+    @FXML
+    private TextField viewText;
+    @FXML
+    private Label timeViewers;
+    @FXML
+    private Label viewersState;
+    @FXML
+    private TextField participantsText;
+    @FXML
+    private Label timeParticipants;
+    @FXML
+    private Label participantsState;
+    @FXML
+    private ImageView avatar;
+    @FXML
+    private Label idLabel;
+    @FXML
+    private Label fnLabel;
+    @FXML
+    private Label lnLabel;
+    @FXML
+    private Label emailLabel;
+    @FXML
+    private Label genderLabel;
+    @FXML
+    private Label ipLabel;
+    @FXML
+    private Label counLabel;
+    @FXML
+    private Label birthLabel;
+    @FXML
+    private Tab tab2;
+    @FXML
+    private AnchorPane drawPane;
+    
+    
+    
+    @FXML
+    void explorate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void load(ActionEvent event) {
+
+    }
+
+    @FXML
+    void participantsDraw(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchParticipants(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchViewers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewersDraw(ActionEvent event) {
+
+    }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+	}
+
+}
+
